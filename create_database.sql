f17315bf28f3fb5ae5fb84dedf7f04bcb272696c
Helen <57248615+mechtal@users.noreply.github.com>
2020-06-18 21:43:45 +0100

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DISEASE](
+    [disease_class_id] [int] NULL,
+    [disease_number] [int] NULL,
+    [disease_name] [varchar](100) NULL
+) ON [PRIMARY]
+GO
